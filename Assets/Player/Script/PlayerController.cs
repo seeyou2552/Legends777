932f41fe4a06155fcfc9d5352291a3a7439741a7
seeyou2552 <dbqlsrnt123@naver.com>
2025-05-09 12:32:10 +0900

    feat : 플레이어 기능 추가

--- Assets/Player/Script/PlayerController.cs
+++ Assets/Player/Script/PlayerController.cs
@@ -1,18 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : Player
 {
-    // Start is called before the first frame update
+    public Rigidbody2D rigid;
+    public SpriteRenderer render;
+    protected AnimationHandle animationHandler;
+    private Camera mainCamera;
+    
+
+
+    void Awake()
+    {
+        animationHandler = GetComponent<AnimationHandle>();
+        rigid = GetComponent<Rigidbody2D>();
+        render = GetComponent<SpriteRenderer>();
+        mainCamera = Camera.main;
+        speed = 10;
+    }
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        PlayerMove();
+        if (mouseWorldPos.x < transform.position.x)
+        {
+            render.flipX = true;
+        }
+        else
+        {
+            render.flipX = false;
+        }
     }
+
+    public void PlayerMove()
+    {
+        Vector2 direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left * speed;
+            render.flipX = true;
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right * speed;
+            render.flipX = false;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up * speed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down * speed;
+        }
+
+        rigid.velocity = new Vector2(direction.x, direction.y);
+        animationHandler.Move(direction);
+    }
+
+
 }
---
+++ Assets/Player/Skill/Script/SkillManager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class SkillManager : Skill
+{
+    public GameObject arrowPrefab;
+    public GameObject bowPrefab;
+    public GameObject target;
+    public float shootInterval = 2f;
+    public float timer;
+    public bool createBow = false;
+
+    void Awake()
+    {
+        target = GameObject.Find("Player");
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+        AutoAttack();
+    }
+
+    void AutoAttack()
+    {
+        if (timer >= shootInterval)
+        {
+            if (createBow)
+            {
+                ShootArrow();
+            }
+            else if (addGhost) GhostShoot();
+            else ShootArrow();
+            
+            timer = 0f;
+        }
+    }
+
+    void ShootArrow()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+
+        // 발사체 생성
+        Vector2 direction = (new Vector2(mousePos.x, mousePos.y) - (Vector2)transform.position).normalized;
+
+        float fullAngle = (arrowCount > 1) ? Mathf.Clamp(15f * (arrowCount - 1), 0f, 90f) : 0f;
+        float startAngle = -fullAngle / 2f;
+
+        for (int i = 0; i < arrowCount; i++)
+        {
+            // 퍼지는 각도 계산
+            float angleOffset = (arrowCount > 1) ? startAngle + (fullAngle / (arrowCount - 1)) * i : 0f;
+
+            // 회전된 방향 벡터 계산
+            float angleInRad = Mathf.Atan2(direction.y, direction.x) + angleOffset * Mathf.Deg2Rad;
+            Vector2 rotatedDirection = new Vector2(Mathf.Cos(angleInRad), Mathf.Sin(angleInRad)).normalized;
+
+            // Z 회전값 계산
+            float zRotation = Mathf.Atan2(rotatedDirection.y, rotatedDirection.x) * Mathf.Rad2Deg - 90f;
+
+            GameObject arrow = Instantiate(arrowPrefab, transform.position, Quaternion.Euler(0f, 0f, zRotation));
+
+            SpriteRenderer renderer = arrow.GetComponent<SpriteRenderer>();
+
+            if (renderer != null)
+            {
+                renderer.flipX = rotatedDirection.x < 0;
+            }
+
+            Rigidbody2D rigid = arrow.GetComponent<Rigidbody2D>();
+            if (rigid != null)
+            {
+                rigid.velocity = rotatedDirection * shootSpeed;
+            }
+        }
+    }
+
+    void GhostShoot()
+    {
+        createBow = true;
+        GameObject bowInstance = Instantiate(bowPrefab, new Vector3(target.transform.position.x + 2f, target.transform.position.y, 0f), Quaternion.identity);
+        StartCoroutine(GhostBow(bowInstance));
+    }
+
+    IEnumerator GhostBow(GameObject bow)
+    {
+        ShootArrow();
+        addGhost = false;
+        yield return new WaitForSeconds(shootInterval);
+        Destroy(bow);
+        addGhost = true;
+        createBow = false;
+    }
+}
