efe3dcb858317790a544cc530fd440ed8b1dfe3d
lsh000219 <147456856+lsh000219@users.noreply.github.com>
2025-05-09 10:37:54 +0900

    feat - 상점 시스템 구현
    수치 상 무기 구매, 장착은 잘 됨,
    무기를 실제로 보이는 건 아직

--- Assets/Player/Script/PlayerController.cs
+++ Assets/Player/Script/PlayerController.cs
@@ -1,18 +1,36 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : SingleTon<PlayerController>
 {
-    // Start is called before the first frame update
-    void Start()
+    private int gold = 1000;
+    private WeaponController weapon;
+
+
+
+    private void Update()
     {
-        
+        Debug.Log(Equip.Num());
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public int Gold
     {
-        
+        get { return gold; }
+        private set { gold = value; }
+    }
+
+    public bool MinusGold(int gold) {
+        if (gold <= Gold) { Gold -= gold; return true; }
+        else { return false; } }
+
+    //public void Equip(WeaponController weapon) { this.weapon = weapon; }
+
+    public WeaponController Equip
+    {
+        get { return weapon; }
+        set { weapon = value; }
     }
 }
---
+++ Assets/Weapon/WeaponController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    private int num, atk, price;
+
+    public WeaponController(int num,int atk, int price) { this.num = num; this.atk = atk; this.price = price; }
+
+    public int Num() { return num; }
+
+    public int Atk() {  return atk; }
+
+    public int Price() { return price; }
+}
