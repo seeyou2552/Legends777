075752457d751396cf84811e1d34292888cebd2f
hhd14725 <127010518+hhd14725@users.noreply.github.com>
2025-05-12 08:48:24 +0900

    feat: UIBase 작업 및 타이틀 추가

---
+++ Assets/UI/Script/UI_StageResult.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_StageResultPopup : UI_Popup
+{
+    public Button option1Button;
+    public Button option2Button;
+    public Button option3Button;
+
+    public void Init(/*List<OptionData> options*/)
+    {
+        // TODO: 버튼 텍스트 설정 및 클릭 리스너 연결
+    }
+}
