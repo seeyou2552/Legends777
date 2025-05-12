94aa354860a941c4f307adb07216c39106c5608b
lsh000219 <147456856+lsh000219@users.noreply.github.com>
2025-05-12 09:30:10 +0900

    Update PlayerController.cs

--- Assets/Player/Script/PlayerController.cs
+++ Assets/Player/Script/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using Unity.VisualScripting;
@@ -9,10 +9,17 @@ using static Unity.Burst.Intrinsics.X86.Avx;
 public class PlayerController : Player
 {
     public static PlayerController Instance { get; set; }
+    public float dashSpeed = 15f;
+    public float dashDuration = 0.2f;
+    public float dashTime;
+    public bool isDashing = false;
+    public float dashCoolTime = 2f; // 대쉬에 주어질 쿨타임 시간
+    public float dashCool = 0f; // 대쉬의 남은 쿨타임 시간
+    public GameObject dashTrailPrefab;
 
     public Rigidbody2D rigid;
     public SpriteRenderer render;
-    protected AnimationHandle animationHandler;
+    protected Animator animation;
     private Camera mainCamera;
     private int gold = 1000;
     private WeaponController weapon;
@@ -21,7 +28,7 @@ public class PlayerController : Player
 
     void Awake()
     {
-        animationHandler = GetComponent<AnimationHandle>();
+        animation = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody2D>();
         render = GetComponent<SpriteRenderer>();
         mainCamera = Camera.main;
@@ -43,7 +50,13 @@ public class PlayerController : Player
     {
         //Debug.Log(Equip.Num());
         Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        PlayerMove();
+
+        if (dashCool > 0f)
+        {
+            dashCool -= Time.deltaTime;
+        }
+
+        PlayerMove(mouseWorldPos);
         if (mouseWorldPos.x < transform.position.x)
         {
             render.flipX = true;
@@ -55,62 +68,115 @@ public class PlayerController : Player
     }
 
 
-    public int Gold
+    public int Gold  
     {
         get { return gold; }
         private set { gold = value; }
     }
 
-    public bool MinusGold(int gold)
+    public bool MinusGold(int gold) // 무기 구매시 골드가 충분한지 확인, 골드 감소
     {
         if (gold <= Gold) { Gold -= gold; return true; }
         else { return false; }
     }
 
 
-    public WeaponController Equip
+    public WeaponController Equip // 무기 프로퍼티
     {
         get { return weapon; }
         private set { weapon = value; }
     }
 
-    public void EquipWeapon(WeaponController weaponController, GameObject gameObject)
+    public void EquipWeapon(WeaponController weaponController, GameObject gameObject)  //무기 장착, 무기 공격력 -> weaponController.Atk();
     {
-        Equip = weaponController;
+        Equip = weaponController;   //무기 장착
 
-        if (weaponPivot != null) { Destroy(weaponPivot); }
-        weaponPivot = Instantiate(gameObject, transform.position, Quaternion.identity);
+        //무기 프리팹 부착 코드 자리
+        //if (weaponPivot != null) { Destroy(weaponPivot); }
+        //weaponPivot = Instantiate(gameObject, transform.position, Quaternion.identity);
     }
 
-    public void PlayerMove()
+    public void QuestClear(int gold) { Gold += gold; }  //퀘스트 클리어시 골드 획득
+
+    public void PlayerMove(Vector3 mousePos)
     {
         Vector2 direction = Vector2.zero;
-
-        if (Input.GetKey(KeyCode.A))
+        if (!isDashing)
         {
-            direction += Vector2.left * speed;
-            render.flipX = true;
+            if (Input.GetKey(KeyCode.A))
+            {
+                direction += Vector2.left * speed;
+                render.flipX = true;
+            }
 
+            if (Input.GetKey(KeyCode.D))
+            {
+                direction += Vector2.right * speed;
+                render.flipX = false;
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                direction += Vector2.up * speed;
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                direction += Vector2.down * speed;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && direction != Vector2.zero && dashCool <= 0f)
+            {
+                gameObject.layer = LayerMask.NameToLayer("DamagedPlayer");
+                PlayerDash(direction);
+            }
+
+            rigid.velocity = new Vector2(direction.x, direction.y);
+        }
+        else
+        {
+            Vector2 dashDir = rigid.velocity.normalized;
+            rigid.velocity = dashDir * dashSpeed;
+            dashTime -= Time.deltaTime;
+            if (dashTime <= 0f)
+            {
+                isDashing = false;
+                gameObject.layer = LayerMask.NameToLayer("Player");
+            }
+            CreateDashTrail(rigid.position, mousePos);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (direction != Vector2.zero)
         {
-            direction += Vector2.right * speed;
-            render.flipX = false;
+            animation.SetBool("IsRun", true);
         }
-
-        if (Input.GetKey(KeyCode.W))
+        else
         {
-            direction += Vector2.up * speed;
+            animation.SetBool("IsRun", false);
         }
+    }
 
-        if (Input.GetKey(KeyCode.S))
+    void PlayerDash(Vector2 direction)
+    {
+        isDashing = true;
+        dashTime = dashDuration;
+        rigid.velocity = Vector2.zero;
+        dashCool = dashCoolTime;
+    }
+
+    void CreateDashTrail(Vector2 position, Vector2 mousePos)
+    {
+        GameObject dashTrail = Instantiate(dashTrailPrefab, position, Quaternion.identity);
+
+        if (mousePos.x < transform.position.x)
         {
-            direction += Vector2.down * speed;
+            dashTrail.transform.localScale = new Vector3(-1f, 1f, 1f);
         }
-
-        rigid.velocity = new Vector2(direction.x, direction.y);
-        animationHandler.Move(direction);
+        else
+        {
+            dashTrail.transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        Destroy(dashTrail, 0.1f);
     }
 }
 
