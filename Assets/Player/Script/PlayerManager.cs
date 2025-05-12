63fe5d92cd0a11dcfd6209ea2bd0cf7adb4e4979
hhd14725 <127010518+hhd14725@users.noreply.github.com>
2025-05-12 14:39:12 +0900

    feat: Player 데미지들어가게 수정 및 UI에 연결

--- Assets/Player/Script/PlayerManager.cs
+++ Assets/Player/Script/PlayerManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 [SerializeField]
@@ -9,12 +10,16 @@ public class PlayerManager : Player
     Animator animation;
     SpriteRenderer renderer;
 
-    [Header("체력설정")]
-    [SerializeField] private int maxHealth = 100;
-    [SerializeField] private int currentHealth;
+    [Header("무적(Invincibility) 설정")]
+    [SerializeField] private float invincibilityDuration = 0.2f;
+    private float invincibilityTimer = 0f;
+
+
+    private int maxHealth;
+    private int currentHealth;
+    private int prevRawHp;
     public int MaxHealth => maxHealth;
     public int CurrentHealth => currentHealth;
-
 
     public event Action<int, int> OnHealthChanged;
     public event Action OnPlayerDie;
@@ -24,36 +29,82 @@ public class PlayerManager : Player
         animation = GetComponent<Animator>();
         renderer = GetComponent<SpriteRenderer>();
         Debug.Log("PlayerManager Awake");
+        maxHealth = Player.Instance.hp;
         currentHealth = maxHealth;
+        prevRawHp = Player.Instance.hp;
         OnHealthChanged?.Invoke(currentHealth, maxHealth);
+
     }
+
+    private void Update()
+    {
+        if (invincibilityTimer > 0f)
+            invincibilityTimer -= Time.deltaTime;
+        int rawHp = Player.Instance.hp;
+        if (rawHp < prevRawHp && invincibilityTimer <= 0f)
+        {
+            int dmg = prevRawHp - rawHp;
+            OnDamage(dmg);
+
+            prevRawHp = Player.Instance.hp;
+        }
+        else
+        {
+            prevRawHp = rawHp;
+        }
+    }
+
+
     void OnTriggerStay2D(Collider2D other)
     {
-       
-        if (other.gameObject.CompareTag("Monster"))
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (invincibilityTimer > 0f)
+            return;
+        // A) 몬스터/보스 몸통 태그가 붙어 있으면
+        if (other.tag == "Monster" || other.tag == "Boss")
         {
-            // 데미지 (Hit)
-            OnDamage(other);
+            int dmg = 0;
+            var ms = other.GetComponent<MonsterStatHandler>();
+            if (ms != null) dmg = ms.Atk;
+            OnDamage(dmg);
+            return;
+        }
+
+        var proj = other.GetComponent<ProjectileController>();
+        if (proj != null)
+        {
+            // proj.monsterController가 null인지 체크
+            var ms = proj.monsterController?.GetComponent<MonsterStatHandler>();
+            int dmg = (ms != null) ? ms.Atk : 0;
+            OnDamage(dmg);
+
+            Destroy(other.gameObject);
+            return;
         }
 
     }
 
-    void OnDamage(Collider2D other)
+    public void OnDamage(int damage)
     {
         Debug.Log("hit");
         animation.SetBool("IsHit", true);
 
         gameObject.layer = LayerMask.NameToLayer("DamagedPlayer");
         renderer.color = new Color(1, 1, 1, 0.4f);
+        invincibilityTimer = invincibilityDuration;
         StartCoroutine(OnInvincibility());
 
-        var stat = other.GetComponent<MonsterStatHandler>();
-        int dmg = stat != null ? stat.Atk : 0;
-
-        currentHealth = Mathf.Max(currentHealth - dmg, 0);
+        currentHealth = Mathf.Max(currentHealth - damage, 0);
+        Player.Instance.hp = currentHealth;
         OnHealthChanged?.Invoke(currentHealth, maxHealth);
+        
 
-        if(currentHealth <= 0)
+        if (currentHealth <= 0)
         {
             OnPlayerDie?.Invoke();
         }
@@ -61,7 +112,7 @@ public class PlayerManager : Player
 
     IEnumerator OnInvincibility()
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(invincibilityDuration);
         gameObject.layer = LayerMask.NameToLayer("Player");
         animation.SetBool("IsHit", false);
         renderer.color = new Color(1, 1, 1, 1);
