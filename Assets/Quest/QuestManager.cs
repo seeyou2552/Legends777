dfe3a64342da24ac6b2224c35f150a001d38e9c2
Im Ye Seul <147143220+Imysss@users.noreply.github.com>
2025-05-12 09:26:22 +0900

    add: using

--- Assets/Quest/QuestManager.cs
+++ Assets/Quest/QuestManager.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestManager : MonoBehaviour
 {
