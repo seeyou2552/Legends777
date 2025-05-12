5e9d937c18f20ad4f99a2bdd94b292d93fc48e28
hhd14725 <127010518+hhd14725@users.noreply.github.com>
2025-05-12 12:31:30 +0900

    feat: UI_HealthBar 연결

--- Assets/Player/Script/PlayerManager.cs
+++ Assets/Player/Script/PlayerManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,14 +9,28 @@ public class PlayerManager : Player
     Animator animation;
     SpriteRenderer renderer;
 
+    [Header("체력설정")]
+    [SerializeField] private int maxHealth = 100;
+    [SerializeField] private int currentHealth;
+    public int MaxHealth => maxHealth;
+    public int CurrentHealth => currentHealth;
+
+
+    public event Action<int, int> OnHealthChanged;
+    public event Action OnPlayerDie;
+
     void Awake()
     {
         animation = GetComponent<Animator>();
         renderer = GetComponent<SpriteRenderer>();
+        Debug.Log("PlayerManager Awake");
+        currentHealth = maxHealth;
+        OnHealthChanged?.Invoke(currentHealth, maxHealth);
     }
     void OnTriggerStay2D(Collider2D other)
     {
-        if(other.gameObject.CompareTag("Monster"))
+       
+        if (other.gameObject.CompareTag("Monster"))
         {
             // 데미지 (Hit)
             OnDamage(other);
@@ -31,6 +46,17 @@ public class PlayerManager : Player
         gameObject.layer = LayerMask.NameToLayer("DamagedPlayer");
         renderer.color = new Color(1, 1, 1, 0.4f);
         StartCoroutine(OnInvincibility());
+
+        var stat = other.GetComponent<MonsterStatHandler>();
+        int dmg = stat != null ? stat.Atk : 0;
+
+        currentHealth = Mathf.Max(currentHealth - dmg, 0);
+        OnHealthChanged?.Invoke(currentHealth, maxHealth);
+
+        if(currentHealth <= 0)
+        {
+            OnPlayerDie?.Invoke();
+        }
     }
 
     IEnumerator OnInvincibility()
